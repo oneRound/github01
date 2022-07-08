aec2e492425fec24ba8b1811741d47002a040bcc
yangmengyuan <1905791391@qq.com>
2022-07-08 16:30:51 +0800

    commit01

--- src/Hello.java
+++ src/Hello.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Hello {
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("123456789");
+        System.out.println("777777777");
+        System.out.println("666666666");
+    }
 }
